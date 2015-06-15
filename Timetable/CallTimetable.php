42496bf3e2a3359da54999c965e27f43df19e07d
Dongjun Lee <ehdwns2045@gmail.com>
2015-06-15 15:28:44 +0900

    modify

--- Timetable/CallTimetable.php
+++ Timetable/CallTimetable.php
@@ -6,6 +6,7 @@
 	
 	$query = $_REQUEST['query'];
 	$user_id = $_REQUEST['user_id'];
+	$today = date("D");
 	
 // 	error_reporting(E_ALL);
 // 	ini_set("display_errors", 1);
@@ -16,7 +17,7 @@
 				
 				" SELECT title, start_time, end_time, day_of_week, color
 				  FROM timetable
-				  WHERE user_id = '".$user_id."'
+				  WHERE user_id = ".$user_id." AND day_of_week = '$today'
 				  ORDER BY start_time ASC"
 		);
 		print_r(  json_encode( $Crew_DB->callTimetables( $resultSet ) ) );
